f350250f84168361ce8b76fad73d40eaa9df8bba
Eric Dowd <dowderic@hotmail.com>
2021-01-26 17:59:05 -0500

    .

--- app.py
+++ app.py
@@ -27,41 +27,39 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
     )
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
 
-session = Session(engine)
+	session = Session(engine)
 
-date = dt.datetime(2016, 8, 23)
+	date = dt.datetime(2016, 8, 23)
 
-plot_results = session.query(Measurement.prcp, Measurement.date).\
-    filter(Measurement.date >= date).\
-    order_by(Measurement.date.desc()).all()
-session.close()
-return jsonify(plot_results)
+	plot_results = session.query(Measurement.prcp, Measurement.date).\
+ 	   filter(Measurement.date >= date).\
+	   order_by(Measurement.date.desc()).all()
+	session.close()
+	return jsonify(plot_results)
 
 @app.route("/api/v1.0/stations")
 def stations():
 
-session = Session(engine)
+	session = Session(engine)
 
-activity=session.query(Station.id,Measurement.station, func.count(Measurement.id)).group_by(Measurement.station).\
-    filter(Station.station==Measurement.station).\
-    order_by(func.count(Measurement.id).desc()).all()
+	activity=session.query(Station.id,Measurement.station, func.count(Measurement.id)).group_by(Measurement.station).\
+	    filter(Station.station==Measurement.station).\
+	    order_by(func.count(Measurement.id).desc()).all()
 
-session.close()
-return jsonify(activity)
+	session.close()
+	return jsonify(activity)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-session = Session(engine)
-date2 = dt.datetime(2016, 8, 18)
-plot_results2 = session.query(Measurement.tobs, Measurement.date).\
-    filter(Measurement.station=='USC00519281', Measurement.date>=date2).all()
+	session = Session(engine)
+	date2 = dt.datetime(2016, 8, 18)
+	plot_results2 = session.query(Measurement.tobs, Measurement.date).\
+ 	   filter(Measurement.station=='USC00519281', Measurement.date>=date2).all()
 
-session.close()
-return jsonify(plot_results2)
+	session.close()
+	return jsonify(plot_results2)
