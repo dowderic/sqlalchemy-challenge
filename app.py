dfd11b933fb43a44032b7bfdaacfc3587cc07b45
Eric Dowd <dowderic@hotmail.com>
2021-01-26 17:46:36 -0500

    .

--- app.py
+++ app.py
@@ -1,15 +1,47 @@
 from flask import Flask, jsonify
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
+
+
+engine = create_engine("sqlite:///./Resources/hawaii.sqlite")
+
+
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 app = Flask(__name__)
 
 
 @app.route("/")
-def home():
-    print("Server received request for 'Home' page...")
-    return "Welcome to my 'Home' page!"
+def welcome():
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
 
 @app.route("/api/v1.0/precipitation")
+def precipitation():
 
+session = Session(engine)
+
+date = dt.datetime(2016, 8, 23)
+
+plot_results = session.query(Measurement.prcp, Measurement.date).\
+    filter(Measurement.date >= date).\
+    order_by(Measurement.date.desc()).all()
+return jsonify(plot_results)
 
 @app.route("/api/v1.0/stations")
 
